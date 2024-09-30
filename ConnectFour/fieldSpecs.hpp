8bffc3a899c3537b2096d39f7cc022f39d05ba67
maureenfioreen <fiorinimauro5@gmail.com>
2024-09-30 17:41:50 +0200

    Cleaned up header file

--- ConnectFour/fieldSpecs.hpp
+++ ConnectFour/fieldSpecs.hpp
@@ -1,14 +1,14 @@
-// This file contains the field specifications
-
 #ifndef FIELD_SPECS
 #define FIELD_SPECS
 
+// This file contains the field specifications
+
+// Variables 
 const int HORIZONTAL_FIELD_SIZE = 7;
 const int VERTICAL_FIELD_SIZE = 6; 
 bool IS_GAME_RUNNING = true;
 bool PLAYER_TURN = true; // Keeps track of which player is playing atm
-
-char FIELD[6][7]; 
+char FIELD[6][7]; // Playing field as a double array since vectors made no sense
 
 // Functions
 extern void printField(); 
